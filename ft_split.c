2220abef914326a9392ce00c933b38d707f1c0d9
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 15:02:29 +0100

    fix split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 17:44:36 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/30 20:07:07 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 15:01:18 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ static char	**ft_fill_res(char const *s, char c, char **res)
 		{
 			if (i == 0 || s[i - 1] == c)
 			{
-				res[j] = malloc((ft_wordlen(s + i, c) + 1) * sizeof(char));
+				res[j] = ft_calloc((ft_wordlen(s + i, c) + 1), sizeof(char));
 				if (res[j++] == NULL)
 				{
 					ft_free_tab_tab(res);
@@ -86,20 +86,16 @@ char	**ft_split(char const *s, char c)
 {
 	char	**res;
 	size_t	nb_words;
-	size_t	i;
 
 	nb_words = ft_get_nb_words(s, c);
 	if (nb_words == 0)
 	{
 		res = malloc(sizeof(char *));
-		res[0] = NULL;
+		res[0] = 0;
 		return (res);
 	}
-	res = malloc((nb_words + 1) * sizeof(char *));
+	res = ft_calloc((nb_words + 1), sizeof(char *));
 	if (res == NULL)
 		return (res);
-	i = 0;
-	while (i < nb_words + 1)
-		res[i++] = NULL;
 	return (ft_fill_res(s, c, res));
 }
