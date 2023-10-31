da84dae3a942849c4b606eaf73a7ceacf39abe1b
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 12:19:49 +0100

    fix isdigit

--- ft_isstr.c
+++ ft_isstr.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 12:01:16 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/30 13:06:52 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 12:19:27 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_isalpha(int c)
 
 int	ft_isdigit(int c)
 {
-	return (c >= '0' || c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
 int	ft_isalnum(int c)
