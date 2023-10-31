de0856a7601004661e4df61f6a4efadfb44fe795
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 13:02:54 +0100

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:44:48 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 12:45:10 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 13:02:28 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,26 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t	i;
 	int		overlap;
 
-	if (dest == NULL || src == NULL)
+	if (dest == NULL && src == NULL && n != 0)
 		return (dest);
+	overlap = 0;
+	i = 0;
 	if (src < dest)
-	{
 		overlap = 1;
+	if (src < dest)
 		i = n;
-	}
-	else
-	{
-		overlap = 0;
-		i = 0;
-	}
 	while ((overlap == 1 && i > 0) || (overlap == 0 && i < n))
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		if (overlap == 1)
+		{
+			((unsigned char *)dest)[i - 1] = ((unsigned char *)src)[i - 1];
 			i--;
+		}
 		else
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i++;
+		}
 	}
 	return (dest);
 }
