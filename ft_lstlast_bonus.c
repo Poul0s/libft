27ecdfecf31811be1ed4c3e96f5538ec8db7fe77
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 12:24:35 +0100

    fix lstlast unused var

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:40:53 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 11:42:33 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 12:24:15 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	size_t	i;
-
 	if (lst == NULL)
 		return (NULL);
-	i = 0;
 	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
