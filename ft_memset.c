1b28e6b3307c7d2802b7941ef3b0cc2ca3916582
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 12:56:56 +0100

    fix memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 13:12:26 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 12:46:04 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 12:56:28 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	i;
 
-	if (s == NULL)
-		return (s);
 	i = 0;
 	while (i < n)
-		((int *)s)[i++] = c;
+		((unsigned char *)s)[i++] = c;
 	return (s);
 }
