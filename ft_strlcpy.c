41bea1a9865d0d502b6d32ef00917a3cd104c58c
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 13:27:11 +0100

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 13:32:43 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 12:46:25 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 13:26:36 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
-	if (dest == NULL || src == NULL)
-		return (0);
+	if (size == 0)
+		return (ft_strlen((char *)src));
 	i = 0;
 	while (i < size - 1 && src[i])
 	{
