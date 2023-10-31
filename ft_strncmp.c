d45930ecbff2e3fdb0308dfa2b785eab9b2b3e4d
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 13:22:06 +0100

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:50:25 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 12:50:32 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 13:21:41 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && s1[i] && s2[i])
+	while (i < n && us1[i] && us2[i])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (us1[i] != us2[i])
+			return (us1[i] - us2[i]);
 		i++;
 	}
 	if (i < n)
-		return (s1[i] - s2[i]);
+		return (us1[i] - us2[i]);
 	return (0);
 }
