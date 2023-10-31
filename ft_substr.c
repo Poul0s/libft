5931923554d1e1f642dd5a929ef7fe904468aec2
Paul Salame <psalame@1A1.42angouleme.fr>
2023-10-31 16:13:57 +0100

    fix norm new line

--- ft_substr.c
+++ ft_substr.c
@@ -6,14 +6,14 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:51:37 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 14:53:49 by psalame          ###   ########.fr       */
+/*   Updated: 2023/10/31 16:12:06 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static char	*ft_empty_str()
+static char	*ft_empty_str(void)
 {
 	char	*str;
 
