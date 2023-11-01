a5a5b4c649371a15261d28a1cb4d2400709162bb
Paul Salame <psalame@1A1.42angouleme.fr>
2023-11-01 13:33:41 +0100

    fix putnbr fd -

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:37:59 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 12:38:20 by psalame          ###   ########.fr       */
+/*   Updated: 2023/11/01 13:33:02 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_putnbr_fd(int n, int fd)
 
 	if (n < 0)
 	{
-		write(1, "-", 1);
+		write(fd, "-", 1);
 		un = -n;
 	}
 	else
