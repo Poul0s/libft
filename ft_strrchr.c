5be87a7a63d5d640440aded74f2724fa259e3dae
Paul Salame <psalame@1A1.42angouleme.fr>
2023-11-01 11:56:05 +0100

    fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:50:56 by psalame           #+#    #+#             */
-/*   Updated: 2023/10/31 13:17:05 by psalame          ###   ########.fr       */
+/*   Updated: 2023/11/01 11:55:23 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 	char	*last_occ;
+	char	chr;
 
 	i = 0;
 	last_occ = NULL;
+	chr = (char) c;
 	while (s[i] != 0)
 	{
-		if (s[i] == c)
+		if (s[i] == chr)
 			last_occ = (char *)s + i;
 		i++;
 	}
-	if (c == 0)
+	if (chr == 0)
 		last_occ = (char *)s + i;
 	return (last_occ);
 }
